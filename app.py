662fd2a9a83b5a980b5673a6949f49ddd40c8df8
Cole Hagen <61998370+chags1313@users.noreply.github.com>
2023-10-01 13:32:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,53 @@
 from flask import Flask, render_template, request, jsonify
-import cv2
-import numpy as np
+import pandas as pd
+import math
 
 app = Flask(__name__)
 
-# Dummy function for image processing (replace with your actual image processing logic)
-def process_image(image_stream):
-    try:
-        # Convert the image stream to a NumPy array
-        image = cv2.imdecode(np.frombuffer(image_stream, np.uint8), -1)
-
-        # Your image processing logic here
-        # For demonstration purposes, we'll apply a simple grayscale filter
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        # Encode the processed image back to a stream
-        _, processed_image = cv2.imencode('.jpg', gray_image)
-
-        return processed_image.tobytes(), []
-
-    except Exception as e:
-        return None, str(e)
+calorie_burn_rate = 0.002  # An arbitrary value representing the calorie burn rate per unit of movement
 
 @app.route('/')
 def index():
-    return render_template('cam.html')
+    return render_template('index.html')
 
-@app.route('/process_image', methods=['POST'])
-def process_image_route():
-    try:
-        image_stream = request.files['image'].read()
-        image_data, keypoints = process_image(image_stream)
-        if image_data:
-            response_data = {
-                'image': image_data.decode('latin1'),  # Convert bytes to string
-                'keypoints': keypoints
-            }
-            return jsonify(response_data)
-        else:
-            return jsonify({'error': 'Image processing failed'})
+@app.route('/process', methods=['POST'])
+def process_keypoints():
+    global prev_keypoints
+    data = request.json
+    keypoints = data.get('keypoints', [])
+    weight = data.get('weight', 70)  # get the user's weight from the request, default to 70kg if not provided
+    
+    # Calculate calories based on the movement
+    if prev_keypoints:  # calculate the movement if we have previous keypoints to compare
+        total_movement = sum(
+            math.sqrt((x['x'] - prev_x['x']) ** 2 + (x['y'] - prev_x['y']) ** 2) 
+            for x, prev_x in zip(keypoints, prev_keypoints)
+        )
+        calories_burned = total_movement * calorie_burn_rate * weight
+    else:
+        calories_burned = 0
+    
+    prev_keypoints = keypoints  # update the previous keypoints
+    print(calories_burned)
+    
+    # Prepare the keypoints data
+    data_df = pd.DataFrame(keypoints)
+    pose_landmarks = [
+        "Nose", "Left Eye Inner", "Left Eye", "Left Eye Outer", "Right Eye Inner",
+        "Right Eye", "Right Eye Outer", "Left Ear", "Right Ear", "Mouth Left",
+        "Mouth Right", "Left Shoulder", "Right Shoulder", "Left Elbow", "Right Elbow",
+        "Left Wrist", "Right Wrist", "Left Pinky", "Right Pinky", "Left Index",
+        "Right Index", "Left Thumb", "Right Thumb", "Left Hip", "Right Hip",
+        "Left Knee", "Right Knee", "Left Ankle", "Right Ankle", "Left Heel",
+        "Right Heel", "Left Foot Index", "Right Foot Index"
+    ]
+    data_df['Pose Landmark'] = pose_landmarks
+    
+    #print(data_df)
+    return jsonify({"status": "success", "calories_burned": calories_burned})
 
-    except Exception as e:
-        return jsonify({'error': str(e)})
+prev_keypoints = None  # to store the previous frame's keypoints
+
 
 if __name__ == '__main__':
     # Use the provided $PORT variable by Heroku
